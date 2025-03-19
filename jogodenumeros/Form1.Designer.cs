d5e34a9c0a6d83471e4082eafcdbe77ee4e4aa32
claramorais25 <maria.csmorais2@senacsp.edu.br>
2025-03-19 17:07:24 -0300

    Sistemas

---
+++ jogodenumeros/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace jogodenumeros
+{
+    partial class frmJogodeNumeros
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblSubtitulo = new System.Windows.Forms.Label();
+            this.txtNumeroInserido = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnTentativas = new System.Windows.Forms.Button();
+            this.lblAbaixoBotao = new System.Windows.Forms.Label();
+            this.lblNumeroTentativas = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Pink;
+            this.panel1.Controls.Add(this.lblTitulo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(147, 31);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(505, 32);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Boas Vindas ao meu jogo de números";
+            // 
+            // lblSubtitulo
+            // 
+            this.lblSubtitulo.AutoSize = true;
+            this.lblSubtitulo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtitulo.Location = new System.Drawing.Point(187, 114);
+            this.lblSubtitulo.Name = "lblSubtitulo";
+            this.lblSubtitulo.Size = new System.Drawing.Size(429, 22);
+            this.lblSubtitulo.TabIndex = 1;
+            this.lblSubtitulo.Text = "10 tentantivas! Insira um número de 1 até 100.";
+            // 
+            // txtNumeroInserido
+            // 
+            this.txtNumeroInserido.Location = new System.Drawing.Point(322, 153);
+            this.txtNumeroInserido.Name = "txtNumeroInserido";
+            this.txtNumeroInserido.Size = new System.Drawing.Size(124, 20);
+            this.txtNumeroInserido.TabIndex = 2;
+            this.txtNumeroInserido.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(178, 400);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(447, 20);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // btnTentativas
+            // 
+            this.btnTentativas.BackColor = System.Drawing.Color.Pink;
+            this.btnTentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTentativas.ForeColor = System.Drawing.Color.Black;
+            this.btnTentativas.Location = new System.Drawing.Point(286, 204);
+            this.btnTentativas.Name = "btnTentativas";
+            this.btnTentativas.Size = new System.Drawing.Size(199, 79);
+            this.btnTentativas.TabIndex = 4;
+            this.btnTentativas.Text = "Tentativas";
+            this.btnTentativas.UseVisualStyleBackColor = false;
+            this.btnTentativas.Click += new System.EventHandler(this.btnTentativas_Click);
+            // 
+            // lblAbaixoBotao
+            // 
+            this.lblAbaixoBotao.AutoSize = true;
+            this.lblAbaixoBotao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbaixoBotao.Location = new System.Drawing.Point(226, 354);
+            this.lblAbaixoBotao.Name = "lblAbaixoBotao";
+            this.lblAbaixoBotao.Size = new System.Drawing.Size(266, 24);
+            this.lblAbaixoBotao.TabIndex = 5;
+            this.lblAbaixoBotao.Text = "Veja quantas tentativas restam:";
+            // 
+            // lblNumeroTentativas
+            // 
+            this.lblNumeroTentativas.AutoSize = true;
+            this.lblNumeroTentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroTentativas.Location = new System.Drawing.Point(487, 354);
+            this.lblNumeroTentativas.Name = "lblNumeroTentativas";
+            this.lblNumeroTentativas.Size = new System.Drawing.Size(30, 24);
+            this.lblNumeroTentativas.TabIndex = 6;
+            this.lblNumeroTentativas.Text = "10";
+            this.lblNumeroTentativas.Click += new System.EventHandler(this.lblNumeroTentativas_Click);
+            // 
+            // frmJogodeNumeros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNumeroTentativas);
+            this.Controls.Add(this.lblAbaixoBotao);
+            this.Controls.Add(this.btnTentativas);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumeroInserido);
+            this.Controls.Add(this.lblSubtitulo);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmJogodeNumeros";
+            this.Text = "Jogo de Números";
+            this.Load += new System.EventHandler(this.frmJogodeNumeros_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblSubtitulo;
+        private System.Windows.Forms.TextBox txtNumeroInserido;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnTentativas;
+        private System.Windows.Forms.Label lblAbaixoBotao;
+        private System.Windows.Forms.Label lblNumeroTentativas;
+    }
+}
+
